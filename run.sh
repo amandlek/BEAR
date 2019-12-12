e0c063d794d00e93dbd7d3fbcd80082d0d51495a
amandlek <ajay.mandlekar@gmail.com>
2019-12-11 23:31:47 -0800

    small change

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # example run
-python main.py --eval_freq=10 --algo_name=BEAR --distance_type=MMD --mode=auto --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name test
+python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name test
