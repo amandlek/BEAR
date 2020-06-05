877e3a3f09078ee3602b29780c7247ac057a5e0e
amandlek <ajay.mandlekar@gmail.com>
2020-06-05 15:09:09 -0700

    uncommitted changes

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 # example run
-python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --eval_freq=1000 --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name test
+python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --eval_freq=1000 --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name bear_lift_gaussian_20_eval_1000
+python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --eval_freq=1000 --batch ~/Desktop/RoboTurkPilot/bins-Can/states_top_225.hdf5 --name bear_cans_gaussian_20_eval_1000
+python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=20.0 --kernel_type=gaussian --eval_freq=1000 --batch ~/Desktop/RoboTurkPilot/bins-Can/states_top_225.hdf5 --name bear_cans_all_gaussian_20_eval_1000
 
-python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=10.0 --kernel_type=laplacian --eval_freq=1000 --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name test
+python main.py --algo_name=BEAR --distance_type=MMD --mode=auto --lagrange_thresh=10.0 --num_samples_match=5 --lamda=0.0 --version=0 --mmd_sigma=10.0 --kernel_type=laplacian --eval_freq=1000 --batch ~/Desktop/LiftDemos/SawyerLiftPhoneSuboptimal/states.hdf5 --name bear_lift_laplacian_20_eval_1000
